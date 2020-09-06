93d0ff9d5fa86e1405396c8b29d2e5892cc27d7a
Andréa Beyer <48089121+AndreaBeyer@users.noreply.github.com>
2020-09-06 20:26:00 +0200

    **

--- PlusOuMoinsIOS/PlusOuMoinsIOS/ViewController.swift
+++ PlusOuMoinsIOS/PlusOuMoinsIOS/ViewController.swift
@@ -15,16 +15,16 @@ class ViewController: UIViewController {
     @IBOutlet var myField : UITextField!
     @IBOutlet var answ : UITextField!
     @IBOutlet var tryF : UITextField!
+    @IBOutlet var index : UISegmentedControl!
     
+    var difficulty = 0
     var nbTry = 0
-    var rand: UInt32 = 0
+    var rand: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        rand = arc4random_uniform(100) + 1
-        print(rand.description)
-        tryF.text = nbTry.description
-        myField.text = "Entrez un nombre entre 1 et 100"
+        newRandom()
+        resetUI()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -37,13 +37,13 @@ class ViewController: UIViewController {
         
         nbTry++
         
-        if(UInt32(answ.text!) == nil){
+        if(Int(answ.text!) == nil){
             myField.text = "Veuillez entrer un nombre"
         }
-        else if(UInt32(answ.text!) > rand){
+        else if(Int(answ.text!) > rand){
             myField.text = "C'est moins"
         }
-        else if(UInt32(answ.text!) < rand){
+        else if(Int(answ.text!) < rand){
             myField.text = "C'est Plus"
         }
         else{
@@ -51,20 +51,57 @@ class ViewController: UIViewController {
             myBtn.enabled = false
             answ.enabled = false
         }
+        
         answ.text = nil
         tryF.text = nbTry.description
     }
     
     @IBAction func clicNG(sender : UIButton){
-        rand = arc4random_uniform(101)
-        nbTry = 0
-        print(rand.description)
+        newRandom()
+        resetUI()
+    }
+    
+    func resetUI(){
         myBtn.enabled = true
         answ.enabled = true
         tryF.text = nbTry.description
-        myField.text = "Entrez un nombre entre 1 et 100"
+        
         answ.text = nil
+    }
+    
+    func newRandom(){
+        switch difficulty{
+        case 0:
+            rand = createIntRandom(1, max: 10)
+            myField.text = "Entrez un nombre entre 1 et 10"
+            break
+        case 1:
+            rand = createIntRandom(1, max: 100)
+            myField.text = "Entrez un nombre entre 1 et 100"
+            break
+        case 2:
+            rand = createIntRandom(1, max: 1000)
+            myField.text = "Entrez un nombre entre 1 et 1000"
+            break
+        default:
+            break
+        }
 
+        nbTry = 0
+        print(rand.description)
+    }
+    
+    func createIntRandom(min: Int, max: Int) -> Int{
+        
+        return Int(arc4random_uniform(UInt32(max-min+1)) + UInt32(min))
+    }
+    
+    @IBAction func indexChanged(sender: UISegmentedControl) {
+        
+        difficulty = sender.selectedSegmentIndex
+        newRandom()
+        resetUI()
+        print(difficulty)
     }
 
 
